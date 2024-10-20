2bc72a3cdcb16e8dd0e8993cff0bdb9a9451f54b
KJH04_ <10wjdgp04@gmail.com>
2024-10-20 14:22:11 +0900

    캔버스 열기/닫기

---
+++ simple_rpg.py
@@ -0,0 +1,50 @@
+from pico2d import *
+
+# Game object class here
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            pass
+
+
+def reset_world():
+    global running
+    global world
+
+    running = True
+    world = []
+    pass
+
+
+def update_world():
+    for o in world:
+        o.update()
+    pass
+
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
+
+
+open_canvas(1200,700)
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
